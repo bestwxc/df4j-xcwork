bd1870475e20a300311f316f8e27456106b07c89
wangxc <wxc252@hotmail.com>
2022-02-12 14:05:04 +0800

    增加常用工具类

--- src/main/java/com/df4j/xcwork/base/exception/BizException.java
+++ src/main/java/com/df4j/xcwork/base/exception/BizException.java
@@ -1,5 +1,7 @@
 package com.df4j.xcwork.base.exception;
 
+import com.df4j.xcwork.base.constants.ErrorCode;
+
 /**
  * 业务异常
  */
@@ -7,6 +9,11 @@ public class BizException extends XcworkException {
 
     // 异常代码
     private String errorCode;
+
+    public BizException(ErrorCode errorCode) {
+        super(errorCode.name());
+        this.errorCode = String.valueOf(errorCode.getErrorNo());
+    }
 
     public BizException(String errorCode) {
         this.errorCode = errorCode;

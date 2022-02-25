80dd80a010ba509cb9369a3a0997fc7d90e4ac7e
wangxc <wxc252@hotmail.com>
2022-02-25 15:35:29 +0800

    调整方法

--- src/main/java/com/df4j/xcwork/servlet/LoginUserUtils.java
+++ src/main/java/com/df4j/xcwork/servlet/LoginUserUtils.java
@@ -9,7 +9,7 @@ public class LoginUserUtils {
 
     private static final String LOGIN_USER_KEY = "XCWORK_LOGIN_USER";
 
-    public LoginUser getLoginUser(HttpServletRequest request) {
+    public static LoginUser getLoginUser(HttpServletRequest request) {
         HttpSession session = SessionUtils.getSession(request);
         if (session != null) {
             Object obj = SessionUtils.getSessionAttribute(session, LOGIN_USER_KEY);
@@ -21,7 +21,7 @@ public class LoginUserUtils {
     }
 
 
-    public void setLoginUser(HttpServletRequest request, LoginUser loginUser) {
+    public static void setLoginUser(HttpServletRequest request, LoginUser loginUser) {
         HttpSession session = SessionUtils.getSession(request);
         SessionUtils.setSessionAttribute(session, LOGIN_USER_KEY, loginUser);
     }
